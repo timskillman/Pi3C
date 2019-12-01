36ea89e362e7cf5ad51e4e9f764c4001eb984ec8
Tim Skillman <tim@skillmanmedia.com>
2019-12-01 23:51:52 +0000

    Added Third party libraries for x86 builds - saves having to download.  Made changes to 3D Modeller VS project

--- SharedCode/helpers/Pi3Ccontour.h
+++ SharedCode/helpers/Pi3Ccontour.h
@@ -1,15 +1,12 @@
 #pragma once
 #include "Pi3Cvector.h"
 #include "Pi3Cbbox2d.h"
-#include "Pi3Ctexture.h"
-
 #include <vector>
-#include <memory>
 
 class Pi3ClinContour
 {
 public:
-	Pi3ClinContour() {}
+	Pi3ClinContour() : hole(0), closed(false), parent(-1) {}
 	~Pi3ClinContour() {}
 	Pi3ClinContour(const std::vector<vec2f> &points);
 
@@ -19,10 +16,10 @@ public:
 
 	std::vector<vec2f> points;
 	Pi3Cbbox2d bbox;
-	//int32 ref;
-	bool closed;
-	int hole;
-	int parent;
+
+	bool closed = false;
+	int hole = 0;
+	int parent = -1;
 	vec2f leftmost;
 };
 
@@ -30,15 +27,11 @@ class Pi3ClinPath {
 public:
 
 	Pi3ClinPath() {}
-	Pi3ClinPath(const char * file) { open(file); }
 
 	std::vector<Pi3ClinContour> linContours;
 	Pi3Cbbox2d bbox;
 
 	void append(const Pi3ClinContour& lcont);
-	void open(const char* filepath);
-	void drawToTexture(std::shared_ptr<Pi3Ctexture> tex, float sc);
-	void fillToTexture(std::shared_ptr<Pi3Ctexture> tex, float sc);
 	std::vector<std::vector<vec2f>> asVectors();
 	std::vector<std::vector<float>> asFloats();
 };
