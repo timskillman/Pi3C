7660fa9c3e6af33df4e56e49fbcf426679608f54
timskillman <timskillman@gmail.com>
2021-01-19 21:48:26 +0000

    Restored Pi3Cjson files

--- SharedCode/file/Pi3Cjson.cpp
+++ SharedCode/file/Pi3Cjson.cpp
@@ -5,107 +5,107 @@ Pi3Cjson::Pi3Cjson()
 {
 	json_string_buffer.Clear();
 	json_writer.Reset(json_string_buffer);
-	json_writer.SetFormatOptions(rapidjson::kFormatSingleLineArray);
+	json_writer.SetFormatOptions(kFormatSingleLineArray);
 }
 
 Pi3Cjson::Pi3Cjson(const char * file)
 {
 	std::ifstream ifs(file);
-	rapidjson::IStreamWrapper isw(ifs);
+	IStreamWrapper isw(ifs);
 	doc.ParseStream(isw);
 }
 
-int Pi3Cjson::readInt(const rapidjson::Value& doc, const char * key)
+int Pi3Cjson::readInt(const Value& doc, const char * key)
 {
 	if (!doc.HasMember(key)) return 0;
 	return doc[key].GetInt();
 }
 
-void Pi3Cjson::readInt2(const rapidjson::Value& doc, const char * key, int32_t &v1, int32_t &v2)
+void Pi3Cjson::readInt2(const Value& doc, const char * key, int32_t &v1, int32_t &v2)
 {
 	if (!doc.HasMember(key)) return;
-	const rapidjson::Value& a = doc[key];
+	const Value& a = doc[key];
 	if (a.Size() != 2) return;
 	v1 = a[0].GetInt();
 	v2 = a[1].GetInt();
 }
-void Pi3Cjson::readInt3(const rapidjson::Value& doc, const char * key, int32_t &v1, int32_t &v2, int32_t &v3)
+void Pi3Cjson::readInt3(const Value& doc, const char * key, int32_t &v1, int32_t &v2, int32_t &v3)
 {
 	if (!doc.HasMember(key)) return;
-	const rapidjson::Value& a = doc[key];
+	const Value& a = doc[key];
 	if (a.Size() != 3) return;
 	v1 = a[0].GetInt();
 	v2 = a[1].GetInt();
 	v3 = a[2].GetInt();
 }
 
-bool Pi3Cjson::readBool(const rapidjson::Value& doc, const char * key)
+bool Pi3Cjson::readBool(const Value& doc, const char * key)
 {
 	if (!doc.HasMember(key)) return false;
 	return doc[key].GetBool();
 }
 
-uint32_t readHexVal(const rapidjson::Value& doc, const char* key)
+uint32_t readHexVal(const Value& doc, const char* key)
 {
 	std::string v = doc[key].GetString();
 	long n = std::stol(v, nullptr, 16);
 	return (uint32_t)n;
 }
 
-uint32_t Pi3Cjson::readHex(const rapidjson::Value& doc, const char * key)
+uint32_t Pi3Cjson::readHex(const Value& doc, const char * key)
 {
 	if (!doc.HasMember(key)) return 0;
 	return readHexVal(doc, key);
 }
 
-uint32_t Pi3Cjson::readHexDefault(const rapidjson::Value& doc, const char* key, uint32_t def)
+uint32_t Pi3Cjson::readHexDefault(const Value& doc, const char* key, uint32_t def)
 {
 	if (!doc.HasMember(key)) return def;
 	return readHexVal(doc, key);
 }
 
-uint32_t Pi3Cjson::readColour(const rapidjson::Value& doc, const char* key, const uint32_t def)
+uint32_t Pi3Cjson::readColour(const Value& doc, const char* key, const uint32_t def)
 {
 	if (!doc.HasMember(key)) return def;
 	return Pi3Ccolours::getPi3Ccolour(doc[key].GetString());
 }
 
-float Pi3Cjson::readFloat(const rapidjson::Value& doc, const char * key)
+float Pi3Cjson::readFloat(const Value& doc, const char * key)
 {
 	if (!doc.HasMember(key)) return 0;
 	return doc[key].GetFloat();
 }
 
-void Pi3Cjson::readFloat2(const rapidjson::Value& doc, const char * key, float& v1, float& v2)
+void Pi3Cjson::readFloat2(const Value& doc, const char * key, float& v1, float& v2)
 {
 	if (!doc.HasMember(key)) return;
-	const rapidjson::Value& a = doc[key];
+	const Value& a = doc[key];
 	if (a.Size() != 2) return;
 	v1 = a[0].GetFloat();
 	v2 = a[1].GetFloat();
 }
 
-vec2f Pi3Cjson::readVec2f(const rapidjson::Value& doc, const char * key)
+vec2f Pi3Cjson::readVec2f(const Value& doc, const char * key)
 {
 	if (!doc.HasMember(key)) return vec2f(0, 0);
-	const rapidjson::Value& a = doc[key];
+	const Value& a = doc[key];
 	if (a.Size() != 2) return vec2f(0, 0);
 	return vec2f((float)a[0].GetDouble(), (float)a[1].GetDouble());
 
 }
-vec3f Pi3Cjson::readVec3f(const rapidjson::Value& doc, const char * key)
+vec3f Pi3Cjson::readVec3f(const Value& doc, const char * key)
 {
 	if (!doc.HasMember(key)) return vec3f(0, 0, 0);
-	const rapidjson::Value& a = doc[key];
+	const Value& a = doc[key];
 	if (a.Size() != 3) return vec3f(0, 0, 0);
 	return vec3f((float)a[0].GetDouble(), (float)a[1].GetDouble(), (float)a[2].GetDouble());
 }
 
-Pi3Cmatrix Pi3Cjson::readMatrix(const rapidjson::Value& doc, const char * key)
+Pi3Cmatrix Pi3Cjson::readMatrix(const Value& doc, const char * key)
 {
 	Pi3Cmatrix matrix;
 	if (!doc.HasMember(key)) return matrix;
-	const rapidjson::Value& a = doc[key];
+	const Value& a = doc[key];
 	if (a.Size() != 12) return matrix;
 	matrix.set((float)a[0].GetDouble(), (float)a[1].GetDouble(), (float)a[2].GetDouble(), 0,
 		(float)a[3].GetDouble(), (float)a[4].GetDouble(), (float)a[5].GetDouble(), 0,
@@ -114,7 +114,7 @@ Pi3Cmatrix Pi3Cjson::readMatrix(const rapidjson::Value& doc, const char * key)
 	return matrix;
 }
 
-std::string Pi3Cjson::readString(const rapidjson::Value& doc, const char * key) {
+std::string Pi3Cjson::readString(const Value& doc, const char * key) {
 	return (doc.HasMember(key)) ? doc[key].GetString() : "";
 }
 
--- SharedCode/file/Pi3Cjson.h
+++ SharedCode/file/Pi3Cjson.h
@@ -10,7 +10,7 @@
 #include <fstream>
 #include <sstream>
 
-//using namespace rapidjson;
+using namespace rapidjson;
 
 class Pi3Cjson {
 public:
@@ -18,19 +18,19 @@ public:
 	Pi3Cjson();
 	Pi3Cjson(const char * file);
 
-	int readInt(const rapidjson::Value& doc, const char * key);
-	void readInt2(const rapidjson::Value& doc, const char * key, int32_t &v1, int32_t &v2);
-	void readInt3(const rapidjson::Value& doc, const char * key, int32_t &v1, int32_t &v2, int32_t &v3);
-	bool readBool(const rapidjson::Value& doc, const char * key);
-	float readFloat(const rapidjson::Value& doc, const char * key);
-	void readFloat2(const rapidjson::Value& doc, const char * key, float& v1, float& v2);
-	vec2f readVec2f(const rapidjson::Value& doc, const char * key);
-	vec3f readVec3f(const rapidjson::Value& doc, const char * key);
-	Pi3Cmatrix readMatrix(const rapidjson::Value& doc, const char * key);
-	uint32_t readHex(const rapidjson::Value& doc, const char * key);
-	uint32_t readHexDefault(const rapidjson::Value& doc, const char* key, uint32_t def);
-	uint32_t readColour(const rapidjson::Value& doc, const char* key, const uint32_t def);
-	std::string readString(const rapidjson::Value& doc, const char * key);
+	int readInt(const Value& doc, const char * key);
+	void readInt2(const Value& doc, const char * key, int32_t &v1, int32_t &v2);
+	void readInt3(const Value& doc, const char * key, int32_t &v1, int32_t &v2, int32_t &v3);
+	bool readBool(const Value& doc, const char * key);
+	float readFloat(const Value& doc, const char * key);
+	void readFloat2(const Value& doc, const char * key, float& v1, float& v2);
+	vec2f readVec2f(const Value& doc, const char * key);
+	vec3f readVec3f(const Value& doc, const char * key);
+	Pi3Cmatrix readMatrix(const Value& doc, const char * key);
+	uint32_t readHex(const Value& doc, const char * key);
+	uint32_t readHexDefault(const Value& doc, const char* key, uint32_t def);
+	uint32_t readColour(const Value& doc, const char* key, const uint32_t def);
+	std::string readString(const Value& doc, const char * key);
 
 	void writeString(const char * key, const char * str);
 	void writeVec3f(const char * key, vec3f& v);
@@ -43,14 +43,14 @@ public:
 	void EndArray() { json_writer.EndArray(); }
 	bool saveFile(const char * file);
 
-	rapidjson::Document doc;
+	Document doc;
 
 private:
 
 	double formatDbl(float v);
 
-	rapidjson::StringBuffer json_string_buffer;
-	rapidjson::PrettyWriter<rapidjson::StringBuffer> json_writer;
+	StringBuffer json_string_buffer;
+	PrettyWriter<StringBuffer> json_writer;
 	
 	
 };
