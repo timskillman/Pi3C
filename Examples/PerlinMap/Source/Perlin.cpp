b8d6f67559f5f1476cbe7a49142f107f427eca99
Tim Skillman <tskillman@silhouettesoftware.com>
2025-06-24 22:41:28 +0100

    Fixing Perlin example for RPi again

--- Examples/PerlinMap/Source/Perlin.cpp
+++ Examples/PerlinMap/Source/Perlin.cpp
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 	Pi3C pi3c(opts.asString("title"), screenWidth, screenHeight, opts.asBool("fullscreen"));
 
 	uint32_t width = 256, height = 256;
-	Pi3Ctexture ptex = Pi3Ctexture();
+	Pi3Ctexture *ptex = new Pi3Ctexture();
 
 	float pz = 50.f;
 	float pamp = 1.f;
@@ -51,20 +51,20 @@ int main(int argc, char *argv[])
 	float freq = 5.f;
 
 	//Pi3Ctexture ptex = Pi3Ctexture(width, height, 4);
-	ptex.create(width, height, 4);
+	ptex->create(width, height, 4);
 	Pi3CperlinNoise pn;
 	uint32_t start_time = SDL_GetTicks();
-	pn.createMap(ptex.GetTexels(), width, height, pz, octaves, pamp, freq);
+	pn.createMap(ptex->GetTexels(), width, height, pz, octaves, pamp, freq);
 	SDL_Log("Perlin time = %d", SDL_GetTicks() - start_time);
 	//ptex->saveAsPNG("pmap.png");
 	
 	vec3f mapPos{ 0, -80.f, 0 };
-	vec3f mapSize = vec3f((float)ptex.GetWidth(), 100.f, (float)ptex.GetHeight());
+	vec3f mapSize = vec3f((float)ptex->GetWidth(), 100.f, (float)ptex->GetHeight());
 
 	int32_t rect = pi3c.create_rect2D(vec2f(30.f, 30.f), vec2f((float)width, (float)height), Pi3Ccolours::White);
-	pi3c.model2D(rect)->addPicture(&pi3c.resource, ptex);
+	pi3c.model2D(rect)->addPicture(&pi3c.resource, *ptex);
 
-	int32_t mapref = pi3c.create_elevationMap(mapPos, mapSize, ptex, 128, 128, Pi3Ccolours::White,"");
+	int32_t mapref = pi3c.create_elevationMap(mapPos, mapSize, *ptex, 128, 128, Pi3Ccolours::White,"");
 
 	pi3c.scene.setPerspective3D(screenWidth, screenHeight, opts.asFloat("perspective"), opts.asFloat("nearz"), opts.asFloat("farz"));
 	pi3c.scene.setFixedLight(0xffffff, vec3f(1500.f, 1500.f, 1500.f)); //transform sun position into scene
@@ -85,9 +85,9 @@ int main(int argc, char *argv[])
 		perlingui.update(pi3c.gui, pamp, pz, freq, octaves, updateMap);
 
 		if (pi3c.window.mouseUp() && updateMap) {
-			pn.createMap(ptex.GetTexels(), width, height, pz, octaves, pamp, freq);
-			ptex.update();
-			pi3c.model3D(mapref)->updateMesh(&pi3c.resource, Pi3Cshapes::elevationMap(ptex, mapPos, mapSize, 128, 128, 0, vec2f(1,1), 0));
+			pn.createMap(ptex->GetTexels(), width, height, pz, octaves, pamp, freq);
+			ptex->update();
+			pi3c.model3D(mapref)->updateMesh(&pi3c.resource, Pi3Cshapes::elevationMap(*ptex, mapPos, mapSize, 128, 128, 0, vec2f(1,1), 0));
 			updateMap = false;
 		}
 
@@ -98,6 +98,8 @@ int main(int argc, char *argv[])
 	}
 
 	SDL_Log("Average FPS = %f ", pi3c.getAverageFPS());
+
+	delete ptex;
 
 	pi3c.window.destroy();
 	return 0;
